96fccabcc284797b51613648a589006842f86e14
Ceazzer <kareem@rocketpunch.dev>
2023-08-23 09:50:39 -0600

    add profile repo methods

--- core/repository/profile_repo.go
+++ core/repository/profile_repo.go
@@ -7,6 +7,8 @@ import (
 
 type ProfileRepository interface {
 	Create(data *domain.Profile) (int64, error)
+	GetAll() ([]*domain.Profile, error)
+	GetByID(id int64) (*domain.Profile, error)
 	Delete(id int64) (int64, error)
 }
 
@@ -21,6 +23,18 @@ func NewProfileRepository() ProfileRepository {
 func (r *profileRepository) Create(data *domain.Profile) (int64, error) {
 	result := r.db.Create(data)
 	return data.ID, result.Error
+}
+
+func (r *profileRepository) GetAll() ([]*domain.Profile, error) {
+	var profiles []*domain.Profile
+	result := r.db.Find(&profiles)
+	return profiles, result.Error
+}
+
+func (r *profileRepository) GetByID(id int64) (*domain.Profile, error) {
+	var profile domain.Profile
+	result := r.db.First(&profile, id)
+	return &profile, result.Error
 }
 
 func (r *profileRepository) Delete(id int64) (int64, error) {
